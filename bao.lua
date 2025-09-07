e9ae61093dea7984cfdf2867e6a9d6687b922cec
khachyum8990-bit <khachyum8990@gmail.com>
2025-09-07 14:52:37 +0700

    Add files via upload

---
+++ bao.lua
@@ -0,0 +1,6 @@
+Config = {
+  namescript = "kun hub",--Tên Script
+  logoscript = "120410093428098",--ID Logo Script
+  tacgia = "bao"-- Tên Tác Giả
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tngaming123/vesion/refs/heads/main/scriptbffree.lua"))()
